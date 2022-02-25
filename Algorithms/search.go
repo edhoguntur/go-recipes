1b1657f421bf78ad06c9f68c1d91e7254a6fb310
edhoguntur <edho.guntur@gmail.com>
2022-02-25 14:37:24 +0700

    Add Fast Linear Search Func

--- Algorithms/search.go
+++ Algorithms/search.go
@@ -19,7 +19,24 @@ func BinarySearch(arr []int, x int) int {
 	return -1
 }
 
+func FastLinearSearch(arr []int, x int) int {
+	i := 0
+	count := len(arr)
+	arr = append(arr, x)
+	for true {
+		if arr[i] == x {
+			if i < count {
+				return i
+			}
+			return -1
+		}
+		i++
+	}
+	return -1
+}
+
 func main() {
-	items := []int{2, 1, 3, 4, 5, 6, 7, 8}
+	items := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	fmt.Println(BinarySearch(items, 1))
+	fmt.Println(FastLinearSearch(items, 2))
 }
--- Algorithms/search_test.go
+++ Algorithms/search_test.go
@@ -9,3 +9,11 @@ func BenchmarkBinarySearch(b *testing.B) {
 		BinarySearch(data, 2)
 	}
 }
+
+func BenchmarkFastLinearSearch(b *testing.B) {
+	// Data Test
+	var data = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12}
+	for i := 0; i < b.N; i++ {
+		FastLinearSearch(data, 2)
+	}
+}
