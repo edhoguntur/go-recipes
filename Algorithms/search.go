a347fe624c56eb5342bf20dc1d1b7785b9b90efb
edhoguntur <edho.guntur@gmail.com>
2022-02-26 18:43:42 +0700

    Add InterpolationSearch

--- Algorithms/search.go
+++ Algorithms/search.go
@@ -35,8 +35,32 @@ func FastLinearSearch(arr []int, x int) int {
 	return -1
 }
 
+//InterpolationSearch works when the array is sorted
+func InterpolationSearch(arr []int, x int) int {
+	low := 0
+	high := len(arr) - 1
+	for (arr[low] < x) && (x < arr[high]) {
+		var mid = low + ((x-arr[low])*(high-low))/(arr[high]-arr[low])
+		if arr[mid] < x {
+			low = mid + 1
+		} else if arr[mid] > x {
+			high = mid - 1
+		} else {
+			return mid
+		}
+	}
+	if arr[low] == x {
+		return low
+	}
+	if arr[high] == x {
+		return high
+	}
+	return -1
+}
+
 func main() {
 	items := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	fmt.Println(BinarySearch(items, 1))
 	fmt.Println(FastLinearSearch(items, 2))
+	fmt.Println(InterpolationSearch(items, 2))
 }
