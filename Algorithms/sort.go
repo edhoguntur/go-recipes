75f30e4b5faf823034d598de903b95c18d2e8288
edhoguntur <edho.guntur@gmail.com>
2022-02-28 16:15:16 +0700

    Add CountingSort Algorithms

--- Algorithms/sort.go
+++ Algorithms/sort.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // Time Complexity 0(n^2)
 // Space Complexity 0(1)
@@ -21,9 +24,51 @@ func BubbleSort(arr []int) []int {
 	return items
 }
 
+// Time Complexity 0(n+k)
+// Space Complexity 0(k)
+
+func CountingSort(arr []int) []int {
+	length := len(arr)
+	items := make([]int, length)
+	copy(items, arr)
+
+	var min = math.MaxInt32
+	var max = math.MinInt32
+	for _, x := range arr {
+		if x > max {
+			max = x
+		}
+		if x < min {
+			min = x
+		}
+	}
+
+	var counts = make([]int, max-min+1)
+	for _, x := range arr {
+		counts[x-min]++
+	}
+
+	var total = 0
+	for i := min; i <= max; i++ {
+		var oldCount = counts[i-min]
+		counts[i-min] = total
+		total += oldCount
+	}
+
+	for _, x := range arr {
+		items[counts[x-min]] = x
+		counts[x-min]++
+	}
+	return items
+}
+
 func main() {
 	items := []int{2, 6, 4, 5, 7, 1, 2}
 	sortItems := BubbleSort(items)
+
+	fmt.Println(sortItems)
+
+	sortItems = CountingSort(items)
 
 	fmt.Println(sortItems)
 }
