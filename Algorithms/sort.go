92a1e4abd44631b082b623571b77ba52ec8b349f
edhoguntur <edho.guntur@gmail.com>
2022-03-01 18:52:46 +0700

    Add MergeSort Algorithms

--- Algorithms/sort.go
+++ Algorithms/sort.go
@@ -62,6 +62,56 @@ func CountingSort(arr []int) []int {
 	return items
 }
 
+// Time Complexity 0(n log(n))
+// Space Complexity 0(n)
+
+func MergeSort(arr []int) []int {
+	items := make([]int, len(arr))
+	copy(items, arr)
+	DoMergeSort(items)
+	return items
+}
+
+func DoMergeSort(items []int) {
+	length := len(items)
+	if length == 1 {
+		return
+	}
+	var lLeft = length / 2
+	var left = make([]int, lLeft)
+	copy(left, items[:lLeft])
+	var lRight = length - lLeft
+	var right = make([]int, lRight)
+	copy(right, items[lLeft:])
+
+	DoMergeSort(left)
+	DoMergeSort(right)
+
+	Merge(left, right, items)
+}
+
+func Merge(left []int, right []int, result []int) {
+	l := 0
+	r := 0
+	i := 0
+
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			result[i] = left[l]
+			l++
+		} else {
+			result[i] = right[r]
+			r++
+		}
+		i++
+	}
+	var length = len(left) - l
+	copy(result[i:i+length], left[l:])
+	i = i + length
+	length = len(right) - r
+	copy(result[i:i+length], right[r:])
+}
+
 func main() {
 	items := []int{2, 6, 4, 5, 7, 1, 2}
 	sortItems := BubbleSort(items)
@@ -70,5 +120,8 @@ func main() {
 
 	sortItems = CountingSort(items)
 
+	fmt.Println(sortItems)
+
+	sortItems = MergeSort(items)
 	fmt.Println(sortItems)
 }
